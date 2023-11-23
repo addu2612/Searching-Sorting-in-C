7b9f5b5148a2e48da543fa57b4e0d596df4cd2d3
Arnav Panicker <117341727+addu2612@users.noreply.github.com>
2023-11-24 00:17:23 +0530

    Update RadixSort.c

--- RadixSort.c
+++ RadixSort.c
@@ -1,108 +1,70 @@
+
 #include <stdio.h>
-#include <stdlib.h>
- 
-struct Node
-{
-    int data;
-    struct Node *next;
-};
- 
-void input(int a[], int n)
-{
-    for (int i = 0; i < n; i++)
-        scanf("%d", &a[i]);
-}
- 
-void display(int a[], int n)
-{
-    for (int i = 0; i < n; i++)
-        printf("%d ", a[i]);
-    printf("\n");
-}
- 
-int countDigits(int n)
-{
-    int c = 0;
-    while (n > 0)
-    {
-        c++;
-        n /= 10;
+
+// Function to get the maximum element from the array
+int getMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
     }
-    return c;
-}
- 
-int findMAX(int a[], int n)
-{
-    int max = a[0];
-    for (int i = 0; i < n; i++)
-        if (a[i] > max)
-            max = a[i];
     return max;
 }
- 
-void Insert(struct Node *BinCell[], int value, int index)
-{
-    struct Node *t = (struct Node *)malloc(sizeof(struct Node));
-    t->data = value;
-    t->next = NULL;
-    if (!BinCell[index])
-        BinCell[index] = t;
-    else
-    {
-        struct Node *head = BinCell[index];
-        while (head->next)
-            head = head->next;
-        head->next = t;
+
+// Using counting sort to sort the elements based on significant places
+void countingSort(int arr[], int n, int exp) {
+    int output[n];
+    int count[10] = {0};
+
+    // Store count of occurrences in count[]
+    for (int i = 0; i < n; i++) {
+        count[(arr[i] / exp) % 10]++;
+    }
+
+    // Change count[i] so that count[i] contains actual position of this digit in output[]
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    // Build the output array
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    // Copy the output array to arr[] so that arr[] contains sorted numbers according to current digit
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
     }
 }
- 
-int Delete(struct Node *BinCell[], int index)
-{
-    struct Node *head = BinCell[index];
-    BinCell[index] = BinCell[index]->next;
-    int x = head->data;
-    free(head);
-    return x;
-}
- 
-void Radix(int a[], int n)
-{
-    int d = countDigits(findMAX(a, n));
-    int i, j, div = 1;
-    struct Node *bins[10]; 
-    for (i = 0; i < 10; i++)
-        bins[i] = NULL;
-    for (int p = 1; p <= d; p++) 
-    {
-        for (i = 0; i < n; i++)
-            Insert(bins, a[i], (a[i] / div) % 10); 
-        i = j = 0;
-        while (i < 10)
-        {
-            while (bins[i] != NULL)
-                a[j++] = Delete(bins, i); 
-            i++;
-        }
-        div = div * 10;
+
+// Radix sort function
+void radixSort(int arr[], int n) {
+    // Find the maximum element to determine the number of digits
+    int max = getMax(arr, n);
+
+    // Perform counting sort for every digit
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        countingSort(arr, n, exp);
     }
 }
- 
-int main()
-{
-    int i, n;
-    printf("Enter your array size: ");
-    scanf("%d", &n);
-    int arr[n];
- 
-    printf("Enter the array elements\n");
-    input(arr, n);
- 
-    printf("Original Array : ");
-    display(arr, n);
- 
-    Radix(arr, n);
- 
-    printf("Sorted Array : ");
-    display(arr, n);
+
+int main() {
+    int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    radixSort(arr, n);
+
+    printf("\nSorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
