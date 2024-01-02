43b6aeeb8f4e493d4249a2d2f3bd8850838df0b5
Arnav Panicker <117341727+addu2612@users.noreply.github.com>
2024-01-02 11:22:49 +0530

    Update RadixSort.c

--- RadixSort.c
+++ RadixSort.c
@@ -1,69 +1,65 @@
+#include<stdio.h>
 
-#include <stdio.h>
-
-// Function to get the maximum element from the array
-int getMax(int arr[], int n) {
-    int max = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
+int MaxNo(int a[],int n){
+    int i,j;
+    int max=0;
+    for(i=0;i<n;i++){
+        if(a[i]>max){
+            max=a[i];
         }
     }
     return max;
 }
 
-// Using counting sort to sort the elements based on significant places
-void countingSort(int arr[], int n, int exp) {
+void CountingSort(int a[], int n, int exp) {
     int output[n];
     int count[10] = {0};
 
-    // Store count of occurrences in count[]
     for (int i = 0; i < n; i++) {
-        count[(arr[i] / exp) % 10]++;
+        count[(a[i] / exp) % 10]++;
     }
-
-    // Change count[i] so that count[i] contains actual position of this digit in output[]
     for (int i = 1; i < 10; i++) {
         count[i] += count[i - 1];
     }
-
-    // Build the output array
     for (int i = n - 1; i >= 0; i--) {
-        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-        count[(arr[i] / exp) % 10]--;
+        output[count[(a[i] / exp) % 10] - 1] = a[i];
+        count[(a[i] / exp) % 10]--;
     }
 
-    // Copy the output array to arr[] so that arr[] contains sorted numbers according to current digit
     for (int i = 0; i < n; i++) {
-        arr[i] = output[i];
+        a[i] = output[i];
     }
 }
 
-// Radix sort function
-void radixSort(int arr[], int n) {
-    // Find the maximum element to determine the number of digits
-    int max = getMax(arr, n);
 
-    // Perform counting sort for every digit
-    for (int exp = 1; max / exp > 0; exp *= 10) {
-        countingSort(arr, n, exp);
+void Radix(int a[],int n){
+    int max=MaxNo(a,n);
+    for(int exp=1;max/exp>0;exp*=10){
+        CountingSort(a,n,exp);
     }
 }
+
 
 int main() {
-    int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int a[100];
+    int n;
+    printf("Enter number of components:");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        printf("Enter element: ");
+        scanf("%d",&a[i]);
+    }
 
     printf("Original array: ");
     for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+        printf("%d ", a[i]);
     }
 
-    radixSort(arr, n);
+    Radix(a, n);
 
     printf("\nSorted array: ");
     for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+        printf("%d ", a[i]);
     }
 
     return 0;
